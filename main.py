289402e6b93bb41bf3c04bea3ab997bb7d8b0284
BryanBradfo <bryanchen1105@gmail.com>
2023-09-04 21:58:15 +0200

    pop-up created which works (we can see through the webcam)

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import cv2
+# 0 because not multiple webcam
+cam = cv2.VideoCapture(0)
+
+# turn on camera
+while cam.isOpened():
+    #read the camera
+    ret, frame = cam.read()
+
+    # how to close the popup
+    if cv2.waitKey(10) == ord('q'):
+        break
+    
+    #show that in the computer
+    cv2.imshow('Security Camera', frame)
